8181394ff1bfac5732e6198570081e3784a30d9b
charmmeshky@gmail.com <charmmeshky@gmail.com>
2025-05-05 12:00:55 +0330

    dibag kardane wordguss

--- React  مفاهیم اصلیه/wordguss/src/App.jsx
+++ React  مفاهیم اصلیه/wordguss/src/App.jsx
@@ -1,23 +1,24 @@
-import Titr from './Titr'
-import Form from './Form'
-import './index.css'
-import { createRef } from 'react'
+import Titr from "./Titr";
+import Form from "./Form";
+import "./index.css";
+import { useRef } from "react";
 
 const App = () => {
-    const myComponentRef = createRef()
 
-    const test = () => {
-        myComponentRef.current.handleName()
-    }
+  const myComponentRef = useRef();
 
-    return(
-        <div className="app">
-            {console.log(myComponentRef)}
-            <Titr/>
-            <Form ref={myComponentRef}/>
-            <button onClick={test}>test</button>
-        </div>
-    )
-}
+  const test = () => {
+    myComponentRef.current.handleName()
+  };
 
-export default App
+  return (
+    <div className="app">
+      {console.log(myComponentRef)}
+      <Titr />
+      <Form ref={myComponentRef} />
+      <button onClick={test}>test</button>
+    </div>
+  );
+};
+
+export default App;
