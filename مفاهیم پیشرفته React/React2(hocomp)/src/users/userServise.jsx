2f27e628d214f9fac47c09b5bfb99a1bef9dd8bd
charmmeshky@gmail.com <charmmeshky@gmail.com>
2025-07-12 17:27:04 +0330

    start of hocomp

---
+++ مفاهیم پیشرفته React/React2(hocomp)/src/users/userServise.jsx
@@ -0,0 +1,60 @@
+import Swal from "sweetalert2";
+import { axiosJp } from "../axios";
+
+export const deleteServise = async (users, setUser, idUser) => {
+  axiosJp.delete(`/users/${idUser}`).then((res) => {
+    console.log(res);
+
+    if (res.status === 200) {
+      const newUsers = users.filter((user) => {
+        return user.id !== idUser;
+      });
+      setUser(newUsers);
+      Swal.fire({
+        title: "! حذف شد",
+        text: "شما این کاربر را حذف کردید",
+        icon: "success",
+      });
+    } else {
+      Swal.fire({
+        title: "Eror",
+        text: "! عملیات با موفقیت انجام نشد",
+        icon: "warning",
+      });
+    }
+  });
+};
+
+export const getUser = async (setUser, setBaseUser) => {
+  axiosJp
+    .get("/users")
+    .then((res) => {
+      setUser(res.data), setBaseUser(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const editServise = async (data, id) => {
+  axiosJp.put(`/users/${id}`, data).then((res) => {
+    console.log(res);
+    Swal.fire({
+      title: "ویرایرش با موفقیت انجام شد",
+      icon: "success",
+      draggable: true,
+    });
+  });
+};
+
+export const add = async (data) => {
+    axiosJp.post('/users', data)
+      .then((res) => {
+        console.log(res)
+        Swal.fire({
+          title: "کاربر با موفقیت اضافه شد",
+          icon: "success",
+          draggable: true
+        });
+      })
+}
---
+++ مفاهیم پیشرفته React/React2(hocomp)/src/users/warning.jsx
@@ -0,0 +1,9 @@
+const Warning = () => {
+    return(
+        <div className="war">
+            <h4>صفحه ویرایش کاربر</h4>
+        </div>
+    )
+}
+
+export default Warning
